5eaa64c169aa3fbcd02a978d2596e098fc906bca
Kittens <becauase@charlesis.me>
2019-05-18 16:20:51 -0500

    Present now renders from the API

--- src/components/Present.vue
+++ src/components/Present.vue
@@ -29,19 +29,9 @@
                   >
                 </div>
                 <br>
-                <p class="title is-size-5">Ted Levy (1999-2001) Artistic Consultant</p>
+                <p class="title is-size-5"> {{posts.title.rendered}} </p>
                 <p class="subtitle">
-                  Ted Levy began his training at The Sammy Dyer School of the Theatre, under the tutelage of Mrs.
-                  Shirley Hall-Bass and Mrs. Muriel Burns Wilson Foster. He intensified his training at Columbia
-                  College of Chicago, and provided opportunity for his professional careerr, which began at the Victory
-                  Gardens Theater. Ted won an Emmy Award for his television debut performance in the PBS Special
-                  "Precious Memories", abd impressed the professional dance world in 1990 with his Broadway debut in
-                  "Black and Blue". He has since collaborated with George C. Wolfe and Gregory Hines on "Jelly's Last
-                  Jam," which won an Outer Critics Circle Award in 1993, and the Antoinette Perry Drama Desk nomination
-                  for choreography. Levy hosted "Ted Levy and Friends," directed by Gregory Hines and was assistant
-                  choreographer to Savion Glover for Broadway's 1996 Tony Award Winner, "Bring in 'da Noise, Bring in
-                  'da Funk." He has perfomed in China with the Chicago Human Rhythm Project as well as taught workshops
-                  and performed with Annual Rhythm World.
+                  {{posts.content.rendered}}
                 </p>
               </div>
             </div>
@@ -53,8 +43,42 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: "Present"
+  name: "Present",
+
+data() {
+      return {
+        posts: {}
+      }
+},
+    
+
+    created() {
+      this.fetchData()
+    },
+
+    watch: {
+      '$route': 'fetchData'
+    },
+
+    methods: {
+      fetchData() {
+        // The API is not SEO friendly so we use the route params(:slug) to make it so
+        // axios.get('http://localhost/index.php/wp-json/wp/v2/posts?slug=' + this.$options.name + '/')
+        axios.get('http://localhost/index.php/wp-json/wp/v2/posts?id=38')
+          .then((resp) => {
+            this.posts = resp.data[0]
+            // console.log(resp)
+          })
+          .catch((err) => {
+            // console.log(err)
+          })
+      }
+
+      
+    }
 };
 </script>
 <style scoped>
